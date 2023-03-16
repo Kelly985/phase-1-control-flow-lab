fd64efe472eff78b60d02ec46f21a08f441f3b41
+ <+>
2023-03-16 20:42:47 +0300

    completed phase1 control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distanceInFeet){
   // Write your code here!
+if (distanceInFeet <= 400) {
+  return "This one is on me!";
 }
 
-function ternaryCheckCity(){
+ else if(distanceInFeet > 2000 && distanceInFeet < 2500) {
+return "I will gladly take your thirty bucks.";
+}
+else if (distanceInFeet > 2500){
+  return "No can do.";
+}
+ }
+
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city == 'NYC'){
+    return "Ok, sounds good.";}
+    else {
+    return "No go.";
+    }
+  }
+
+
+
+function switchOnCharmFromTip(tipAmount){
+  // Write your code here!
+  switch (tipAmount) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
+
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
